5c5d02b7b4f7d7ec127356e6a50de3906b1db4c7
luckyone <luckyone1221@gmail.com>
2020-06-07 23:14:04 +0300

    made icon

--- public/js/common.js
+++ public/js/common.js
@@ -7,15 +7,58 @@ function eventHandler() {
 		var mailVal = this.email.value;
 
 		if (mailVal === '') {
-			//Пожалуйста, заполните все обязательные поля
 			ShowWarning('Please, fill all required input fields');
+			event.preventDefault();
+			return;
 		}
 
-		event.preventDefault();
+		if (!validateEmail(mailVal)) {
+			ShowWarning('Please, input correct email address');
+			event.preventDefault();
+			return;
+		}
 	});
 
+	function validateEmail(email) {
+		var re = /\S+@\S+\.\S+/;
+		return re.test(email);
+	}
+
+	var selfCloseTimeOut;
+
 	function ShowWarning(message) {
-		console.log(message);
+		//remove prev interval
+		clearTimeout(selfCloseTimeOut); //close block
+
+		$('.headerBlock__error-bl').fadeOut(function () {
+			var self = this;
+			$(this).removeClass('active'); //change its message
+
+			var innerTxt = this.querySelector('.headerBlock__error-txt');
+
+			if (message) {
+				innerTxt.innerHTML = message;
+			} else {
+				innerTxt.innerHTML = 'Something went wrong...';
+			} //make cross btn work and show block
+
+
+			$(this).fadeIn(function () {
+				$(this).addClass('active');
+				selfCloseTimeOut = window.setTimeout(function () {
+					$(self).fadeOut(function () {
+						$(self).removeClass('active');
+					});
+				}, 3000);
+			});
+			var crossBtn = this.querySelector('.headerBlock__cross-btn');
+			$(crossBtn).click(function () {
+				clearTimeout(selfCloseTimeOut);
+				$(self).fadeOut(function () {
+					$(this).removeClass('active');
+				});
+			});
+		});
 	}
 }
 
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -3,14 +3,66 @@ const $ = jQuery;
 function eventHandler() {
 	$('.headerBlock__email-form').submit(function () {
 		let mailVal = this.email.value;
+
 		if (mailVal === ''){
-			//Пожалуйста, заполните все обязательные поля
-			ShowWarning('Please, fill all required input fields')
+			ShowWarning('Please, fill all required input fields');
+			event.preventDefault();
+			return
 		}
-		event.preventDefault();
+
+		if (!validateEmail(mailVal)){
+			ShowWarning('Please, input correct email address');
+			event.preventDefault();
+			return
+		}
+
 	});
+
+	function validateEmail(email){
+		var re = /\S+@\S+\.\S+/;
+		return re.test(email);
+	}
+
+	let selfCloseTimeOut;
 	function ShowWarning(message){
-		console.log(message);
+		//remove prev interval
+		clearTimeout(selfCloseTimeOut);
+		//close block
+		$('.headerBlock__error-bl').fadeOut(function () {
+			let self = this;
+			$(this).removeClass('active');
+			//change its message
+			let innerTxt = this.querySelector('.headerBlock__error-txt');
+
+			if (message){
+				innerTxt.innerHTML = message;
+			}
+			else{
+				innerTxt.innerHTML = 'Something went wrong...';
+			}
+
+			//make cross btn work and show block
+			$(this).fadeIn(function () {
+				$(this).addClass('active');
+
+				selfCloseTimeOut = window.setTimeout(function () {
+					$(self).fadeOut(function () {
+						$(self).removeClass('active');
+					});
+				}, 3000);
+
+			});
+
+
+			let crossBtn = this.querySelector('.headerBlock__cross-btn');
+			$(crossBtn).click(function () {
+				clearTimeout(selfCloseTimeOut);
+				$(self).fadeOut(function () {
+					$(this).removeClass('active');
+				})
+			});
+
+		});
 	}
 };
 
